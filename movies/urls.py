52b32a04bad98c2969e09060d6556efa53fe0add
Sumr0ck <sumrock1@mail.ru>
2022-03-03 17:50:12 +0300

    update sidebar

--- movies/urls.py
+++ movies/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.MovieView.as_view(), name='main'),
+    path('filter/', views.FilterMoviesView.as_view(), name='filter'),
     path('<slug:slug>/', views.MovieDetailView.as_view(), name='moviesingle'),
     path('review/<int:pk>/', views.AddReview.as_view(), name='add_review'),
+    path('actor/<str:slug>/', views.ActorView.as_view(), name='actor_detail'),
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,11 +1,21 @@
+from django.db.models import Q
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView, View
 
-from .models import Movie, Reviews, Category
+from .models import Movie, Category, Actor, Genre
 from .forms import ReviewForm
 
 # Create your views here.
-class MovieView(ListView):
+class GenreYear:
+    """Жанры и года"""
+    def get_genre(self):
+        return Genre.objects.all()
+
+    def get_year(self):
+        return Movie.objects.filter(draft=False).values('year')
+
+
+class MovieView(GenreYear, ListView):
     """Список фильмов"""
     model = Movie
     queryset = Movie.objects.filter(draft=False)
@@ -13,7 +23,7 @@ class MovieView(ListView):
     # template_name = 'movies/movie_list.html'
 
 
-class MovieDetailView(DetailView):
+class MovieDetailView(GenreYear, DetailView):
     """Полное описание фильма"""
     model = Movie
     slug_field = 'url'
@@ -33,3 +43,20 @@ class AddReview(View):
             form.movie = movie
             form.save()
         return redirect(movie.get_absolute_url())
+
+
+class ActorView(GenreYear, DetailView):
+    """Вывод информации о актере"""
+    model = Actor
+    template_name = 'movies/actor.html'
+    slug_field = 'name'
+
+
+class FilterMoviesView(GenreYear, ListView):
+    """Фильтр фильмов"""
+    def get_queryset(self):
+        queryset = Movie.objects.filter(
+            Q(year__in=self.request.GET.getlist('year')) |
+            Q(genres__in=self.request.GET.getlist('genres'))
+        )
+        return queryset
